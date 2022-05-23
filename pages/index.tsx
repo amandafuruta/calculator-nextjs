14e5797bcdf9003a4e6203814f097c6b3845ced0
Amanda Arissa Furuta <m309166@alunos.ipb.pt>
2022-05-23 08:02:26 -0300

    first commit

---
+++ pages/index.tsx
@@ -0,0 +1,9 @@
+import {HomeStyle} from '../styles/home'
+
+export default function Home() {
+  return (
+    <HomeStyle>
+        <h1>Home</h1>
+    </HomeStyle>
+  )
+}
---
+++ styles/home.ts
@@ -0,0 +1,10 @@
+import styled from 'styled-components'
+
+export const HomeStyle = styled.section`
+    background-image: url("/images/bg.jpg") ;
+    background-repeat: no-repeat;
+    background-size: cover;
+    background-position: center;
+
+   
+`
